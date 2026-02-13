ba017e2a69999efd632791b5e56ef331dbfdd394
Ollie Branch <ollieevelon@gmail.com>
2026-02-13 04:28:46 -0500

    added content, initialized go with hello world gin

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-
 import (
-	"os"
-	"text/template"
-	"net/http"
-	"fmt"
-	"bytes"
     "github.com/gin-gonic/gin"
+    "net/http"
 )
 
+func main() {
+    // Create a default gin router
+    router := gin.Default()
+
+    // Define a route for the root path
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "Quick and Dirty HTMX+Go",
+        })
+    })
+
+    // Start the server on port 9001
+    router.Run(":9001")
+}
